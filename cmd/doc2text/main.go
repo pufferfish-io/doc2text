c276b7f95e4f155b3c4b17be019b1e8e19ce3e2a
Shamil Alibekov <shamil@MacBook-Pro-Shamil.local>
2025-09-13 21:51:44 +0300

    feat(doc2text): refactor import paths for infrastructure adapters to improve organization feat(doc2text): implement Base64FileConverter for converting files to Base64 format fix(doc2text): ensure context cancellation is handled during file conversion process
    feat(s3): implement S3 downloader service for file retrieval and metadata
    feat(yocr): create Yandex OCR recognizer service for text extraction
    feat(zaplogger): add Zap logger service for structured logging in the application

--- cmd/doc2text/main.go
+++ cmd/doc2text/main.go
@@ -1,51 +1,51 @@
 package main
 
 import (
-    "context"
-    "doc2text/internal/core/abstraction/convert"
-    "doc2text/internal/core/abstraction/cqrs"
-    "doc2text/internal/core/abstraction/download"
-    "doc2text/internal/core/abstraction/logger"
-    "doc2text/internal/core/abstraction/recognize"
-    "doc2text/internal/core/usecase/extracttext"
-    "doc2text/internal/infrastructure/adapter/nativeconv"
-    "doc2text/internal/infrastructure/adapter/s3"
-    "doc2text/internal/infrastructure/adapter/yocr"
-    "doc2text/internal/infrastructure/adapter/zaplogger"
-    "doc2text/internal/presentation/api"
-    config "doc2text/internal/presentation/config"
-    ocrv1 "doc2text/internal/presentation/proto/ocr/v1"
-    "doc2text/internal/presentation/server/ocr/v1"
-    "log"
-    "net"
-    "net/http"
-    "os"
-    "os/signal"
-    "syscall"
-    "time"
+	"context"
+	"doc2text/internal/core/abstraction/convert"
+	"doc2text/internal/core/abstraction/cqrs"
+	"doc2text/internal/core/abstraction/download"
+	"doc2text/internal/core/abstraction/logger"
+	"doc2text/internal/core/abstraction/recognize"
+	"doc2text/internal/core/usecase/extracttext"
+	"doc2text/internal/infrastructure/nativeconv"
+	"doc2text/internal/infrastructure/s3"
+	"doc2text/internal/infrastructure/yocr"
+	"doc2text/internal/infrastructure/zaplogger"
+	"doc2text/internal/presentation/api"
+	config "doc2text/internal/presentation/config"
+	ocrv1 "doc2text/internal/presentation/proto/ocr/v1"
+	"doc2text/internal/presentation/server/ocr/v1"
+	"log"
+	"net"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
-    "google.golang.org/grpc"
+	"google.golang.org/grpc"
 )
 
 func main() {
-    cfg := mustLoadConfig()
+	cfg := mustLoadConfig()
 
-    logger, clean := registerLogger()
-    defer clean()
+	logger, clean := registerLogger()
+	defer clean()
 
-    convertor := registerConverter()
+	convertor := registerConverter()
 
-    downloader := registerDownloader(cfg, logger)
+	downloader := registerDownloader(cfg, logger)
 
-    recognizer := registerRecognizer(cfg)
+	recognizer := registerRecognizer(cfg)
 
-    bus := registerCqrs(CqrsOptions{Logger: logger, Converter: convertor, Downloader: downloader, Recognizer: recognizer})
+	bus := registerCqrs(CqrsOptions{Logger: logger, Converter: convertor, Downloader: downloader, Recognizer: recognizer})
 
-    grpcSrv := startGRPCServer(cfg.Server.Addr, bus, logger)
+	grpcSrv := startGRPCServer(cfg.Server.Addr, bus, logger)
 
-    httpSrv := startHTTPServer(cfg.HTTP.Addr, cfg.HTTP.HealthCheckPath, logger)
+	httpSrv := startHTTPServer(cfg.HTTP.Addr, cfg.HTTP.HealthCheckPath, logger)
 
-    waitForShutdown(logger, grpcSrv, httpSrv)
+	waitForShutdown(logger, grpcSrv, httpSrv)
 }
 
 func mustLoadConfig() *config.Config {
@@ -58,110 +58,110 @@ func mustLoadConfig() *config.Config {
 }
 
 func startGRPCServer(addr string, bus *cqrs.Bus, l logger.Logger) *grpc.Server {
-    lis, err := net.Listen("tcp", addr)
-    if err != nil {
-        l.Error("listen: %v", err)
-        os.Exit(1)
-    }
+	lis, err := net.Listen("tcp", addr)
+	if err != nil {
+		l.Error("listen: %v", err)
+		os.Exit(1)
+	}
 
-    grpcSrv := grpc.NewServer()
-    ocrv1.RegisterOcrServiceServer(grpcSrv, ocr.New(bus))
+	grpcSrv := grpc.NewServer()
+	ocrv1.RegisterOcrServiceServer(grpcSrv, ocr.New(bus))
 
-    l.Info("gRPC listening on %s", addr)
-    go func() {
-        if err := grpcSrv.Serve(lis); err != nil {
-            l.Error("serve: %v", err)
-        }
-    }()
-    return grpcSrv
+	l.Info("gRPC listening on %s", addr)
+	go func() {
+		if err := grpcSrv.Serve(lis); err != nil {
+			l.Error("serve: %v", err)
+		}
+	}()
+	return grpcSrv
 }
 
 func registerLogger() (logger.Logger, func()) {
-    logger, cleanup := zaplogger.NewZapLogger()
-    return logger, cleanup
+	logger, cleanup := zaplogger.NewZapLogger()
+	return logger, cleanup
 }
 
 func registerConverter() convert.FileConverter {
-    converter := nativeconv.NewFileConverter()
-    return converter
+	converter := nativeconv.NewFileConverter()
+	return converter
 }
 func registerDownloader(cfg *config.Config, l logger.Logger) download.Downloader {
-    downloader, err := s3.NewDownloader(s3.Config{
-        Endpoint:  cfg.S3.Endpoint,
-        AccessKey: cfg.S3.AccessKey,
-        SecretKey: cfg.S3.SecretKey,
-        Bucket:    cfg.S3.Bucket,
-        UseSSL:    cfg.S3.UseSSL,
-    })
-    if err != nil {
-        l.Error("s3.NewDownloader: %v", err)
-        os.Exit(1)
-    }
-    return downloader
+	downloader, err := s3.NewDownloader(s3.Config{
+		Endpoint:  cfg.S3.Endpoint,
+		AccessKey: cfg.S3.AccessKey,
+		SecretKey: cfg.S3.SecretKey,
+		Bucket:    cfg.S3.Bucket,
+		UseSSL:    cfg.S3.UseSSL,
+	})
+	if err != nil {
+		l.Error("s3.NewDownloader: %v", err)
+		os.Exit(1)
+	}
+	return downloader
 }
 func registerRecognizer(cfg *config.Config) recognize.Recognizer {
-    recognizer := yocr.New(yocr.Options{
-        OcrEndpoint: cfg.Yandex.Endpoint,
-        ApiKey:      cfg.Yandex.APIKey,
-        FolderID:    cfg.Yandex.FolderID,
-        Model:       cfg.Yandex.Model,
-        Languages:   cfg.Yandex.Languages,
-    })
-    return recognizer
+	recognizer := yocr.New(yocr.Options{
+		OcrEndpoint: cfg.Yandex.Endpoint,
+		ApiKey:      cfg.Yandex.APIKey,
+		FolderID:    cfg.Yandex.FolderID,
+		Model:       cfg.Yandex.Model,
+		Languages:   cfg.Yandex.Languages,
+	})
+	return recognizer
 }
 
 type CqrsOptions struct {
-    Logger     logger.Logger
-    Converter  convert.FileConverter
-    Downloader download.Downloader
-    Recognizer recognize.Recognizer
+	Logger     logger.Logger
+	Converter  convert.FileConverter
+	Downloader download.Downloader
+	Recognizer recognize.Recognizer
 }
 
 func registerCqrs(o CqrsOptions) *cqrs.Bus {
-    extractH := extracttext.NewHandler(o.Converter, o.Downloader, o.Logger, o.Recognizer)
-    bus := cqrs.NewBus()
-    cqrs.RegisterQuery(bus, extractH)
-    return bus
+	extractH := extracttext.NewHandler(o.Converter, o.Downloader, o.Logger, o.Recognizer)
+	bus := cqrs.NewBus()
+	cqrs.RegisterQuery(bus, extractH)
+	return bus
 }
 
 func startHTTPServer(httpAddr string, healthPath string, l logger.Logger) *http.Server {
-    httpMux := api.NewRouter(api.Options{HealthCheckPath: healthPath})
-    srv := &http.Server{Addr: httpAddr, Handler: httpMux}
-    l.Info("HTTP listening on %s (health: %s)", httpAddr, healthPath)
-    go func() {
-        if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-            l.Error("http serve: %v", err)
-        }
-    }()
-    return srv
+	httpMux := api.NewRouter(api.Options{HealthCheckPath: healthPath})
+	srv := &http.Server{Addr: httpAddr, Handler: httpMux}
+	l.Info("HTTP listening on %s (health: %s)", httpAddr, healthPath)
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			l.Error("http serve: %v", err)
+		}
+	}()
+	return srv
 }
 
 func waitForShutdown(l logger.Logger, grpcSrv *grpc.Server, httpSrv *http.Server) {
-    c := make(chan os.Signal, 1)
-    signal.Notify(c, os.Interrupt, syscall.SIGTERM)
-    l.Info("Service started; press Ctrl+C to stop")
-    <-c
-    l.Info("Shutdown signal received, exiting")
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	l.Info("Service started; press Ctrl+C to stop")
+	<-c
+	l.Info("Shutdown signal received, exiting")
 
-    ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-    defer cancel()
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
 
-    if httpSrv != nil {
-        if err := httpSrv.Shutdown(ctx); err != nil {
-            l.Error("http shutdown: %v", err)
-        }
-    }
+	if httpSrv != nil {
+		if err := httpSrv.Shutdown(ctx); err != nil {
+			l.Error("http shutdown: %v", err)
+		}
+	}
 
-    if grpcSrv != nil {
-        done := make(chan struct{})
-        go func() {
-            grpcSrv.GracefulStop()
-            close(done)
-        }()
-        select {
-        case <-done:
-        case <-ctx.Done():
-            grpcSrv.Stop()
-        }
-    }
+	if grpcSrv != nil {
+		done := make(chan struct{})
+		go func() {
+			grpcSrv.GracefulStop()
+			close(done)
+		}()
+		select {
+		case <-done:
+		case <-ctx.Done():
+			grpcSrv.Stop()
+		}
+	}
 }
